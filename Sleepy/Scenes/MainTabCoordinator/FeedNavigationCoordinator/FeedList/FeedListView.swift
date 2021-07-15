aaeef04f25c81e3dff7dee32b69b7d7393b0d365
Anas Ben Mustafa <57250290+arabianprince23@users.noreply.github.com>
2021-07-15 20:22:57 +0300

    Написал проверку данных. Добавил функции

--- Sleepy/Scenes/MainTabCoordinator/FeedNavigationCoordinator/FeedList/FeedListView.swift
+++ Sleepy/Scenes/MainTabCoordinator/FeedNavigationCoordinator/FeedList/FeedListView.swift
@@ -19,6 +19,11 @@ struct FeedListView: View {
     @State private var maxHeartRate: String = ""
     @State private var minHeartRate: String = ""
 
+    @State private var phasesData: [Double] = [0] // Чтобы массив не был пустым и пока отображалась карточка захардкоженная
+
+    @State private var showHeartCard: Bool = false
+    @State private var showPhasesCard: Bool = false
+
     // MARK: Views
 
     var body: some View {
@@ -40,83 +45,74 @@ struct FeedListView: View {
                                             fallingAsleepDuration: fallAsleepDuration)
                             .roundedCardBackground(color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .card(.cardBackgroundColor)))
 
-                        CardNameTextView(text: "Sleep session",
-                                         color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .textsColors(.standartText)))
+                        if showPhasesCard {
+                            CardNameTextView(text: "Sleep session",
+                                             color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .textsColors(.standartText)))
 
-                        CardWithChartView(colorProvider: viewModel.colorProvider,
-                                          systemImageName: "sleep",
-                                          titleText: "Sleep: phases",
-                                          mainTitleText: "Here is the info about phases of your last sleep.",
-                                          titleColor: viewModel.colorProvider.sleepyColorScheme.getColor(of: .phases(.deepSleepColor)),
-                                          chartView: StandardChartView(colorProvider: viewModel.colorProvider,
-                                                                       chartType: .phasesChart,
-                                                                       chartHeight: 75,
-                                                                       points: [0.75, 1, 0.3, 0.45, 0.2, 0.35, 0.45, 0.25, 0.65, 0.8, 0.85, 0.9, 0.45, 0.1, 0.65, 0.45, 0.2, 0.35, 0.5, 0.2, 0.6, 0.8, 1],
-                                                                       dragGestureData: "",
-                                                                       chartColor: nil,
-                                                                       needOXLine: true,
-                                                                       needTimeLine: true,
-                                                                       startTime: "23:00",
-                                                                       endTime: "6:54",
-                                                                       needDragGesture: false),
-                                          bottomView: CardBottomSimpleDescriptionView(colorProvider: viewModel.colorProvider,
-                                                                                      descriptionText:
-                                                                                        Text("The duration of light phase was ")
-                                                                                      + Text("3h 45min").foregroundColor(viewModel.colorProvider.sleepyColorScheme.getColor(of: .phases(.lightSleepColor))).bold()
-                                                                                      + Text(", while the duration of deep phase was ")
-                                                                                      + Text("2h 12min").foregroundColor(viewModel.colorProvider.sleepyColorScheme.getColor(of: .phases(.deepSleepColor))).bold()
-                                                                                      + Text(".")))
-                            .roundedCardBackground(color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .card(.cardBackgroundColor)))
+                            CardWithChartView(colorProvider: viewModel.colorProvider,
+                                              systemImageName: "sleep",
+                                              titleText: "Sleep: phases",
+                                              mainTitleText: "Here is the info about phases of your last sleep.",
+                                              titleColor: viewModel.colorProvider.sleepyColorScheme.getColor(of: .phases(.deepSleepColor)),
+                                              chartView: StandardChartView(colorProvider: viewModel.colorProvider,
+                                                                           chartType: .phasesChart,
+                                                                           chartHeight: 75,
+                                                                           points: [0.75, 1, 0.3, 0.45, 0.2, 0.35, 0.45, 0.25, 0.65, 0.8, 0.85, 0.9, 0.45, 0.1, 0.65, 0.45, 0.2, 0.35, 0.5, 0.2, 0.6, 0.8, 1],
+                                                                           dragGestureData: "",
+                                                                           chartColor: nil,
+                                                                           needOXLine: true,
+                                                                           needTimeLine: true,
+                                                                           startTime: "23:00",
+                                                                           endTime: "6:54",
+                                                                           needDragGesture: false),
+                                              bottomView: CardBottomSimpleDescriptionView(colorProvider: viewModel.colorProvider,
+                                                                                          descriptionText:
+                                                                                            Text("The duration of light phase was ")
+                                                                                          + Text("3h 45min").foregroundColor(viewModel.colorProvider.sleepyColorScheme.getColor(of: .phases(.lightSleepColor))).bold()
+                                                                                          + Text(", while the duration of deep phase was ")
+                                                                                          + Text("2h 12min").foregroundColor(viewModel.colorProvider.sleepyColorScheme.getColor(of: .phases(.deepSleepColor))).bold()
+                                                                                          + Text(".")))
+                                .roundedCardBackground(color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .card(.cardBackgroundColor)))
+                        }
 
-                        CardNameTextView(text: "Heart rate",
-                                         color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .textsColors(.standartText)))
+                        if showHeartCard {
+                            CardNameTextView(text: "Heart rate",
+                                             color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .textsColors(.standartText)))
 
-                        CardWithChartView(colorProvider: viewModel.colorProvider,
-                                          systemImageName: "suit.heart.fill",
-                                          titleText: "Sleep: heart rate",
-                                          mainTitleText: "Here is the info about heart rate of your last sleep.",
-                                          titleColor: viewModel.colorProvider.sleepyColorScheme.getColor(of: .heart(.heartColor)),
-                                          chartView: StandardChartView(colorProvider: viewModel.colorProvider,
-                                                                       chartType: .defaultChart,
-                                                                       chartHeight: 75,
-                                                                       points: heartRateData,
-                                                                       dragGestureData: "",
-                                                                       chartColor: viewModel.colorProvider.sleepyColorScheme.getColor(of: .heart(.heartColor)),
-                                                                       needOXLine: true,
-                                                                       needTimeLine: true,
-                                                                       startTime: sleepStart,
-                                                                       endTime: sleepEnd,
-                                                                       needDragGesture: false),
-                                          bottomView: CardBottomSimpleDescriptionView(colorProvider: viewModel.colorProvider,
-                                                                                      descriptionText:
-                                                                                        Text("The maximal heartbeat was ")
-                                                                                      + Text("\(maxHeartRate) bpm").foregroundColor(viewModel.colorProvider.sleepyColorScheme.getColor(of: .heart(.heartColor))).bold()
-                                                                                      + Text(", while the minimal was ")
-                                                                                      + Text("\(minHeartRate) bpm").foregroundColor(viewModel.colorProvider.sleepyColorScheme.getColor(of: .heart(.heartColor))).bold()
-                                                                                      + Text(".")))
-                            .roundedCardBackground(color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .card(.cardBackgroundColor)))
+                            CardWithChartView(colorProvider: viewModel.colorProvider,
+                                              systemImageName: "suit.heart.fill",
+                                              titleText: "Sleep: heart rate",
+                                              mainTitleText: "Here is the info about heart rate of your last sleep.",
+                                              titleColor: viewModel.colorProvider.sleepyColorScheme.getColor(of: .heart(.heartColor)),
+                                              chartView: StandardChartView(colorProvider: viewModel.colorProvider,
+                                                                           chartType: .defaultChart,
+                                                                           chartHeight: 75,
+                                                                           points: heartRateData,
+                                                                           dragGestureData: "",
+                                                                           chartColor: viewModel.colorProvider.sleepyColorScheme.getColor(of: .heart(.heartColor)),
+                                                                           needOXLine: true,
+                                                                           needTimeLine: true,
+                                                                           startTime: sleepStart,
+                                                                           endTime: sleepEnd,
+                                                                           needDragGesture: false),
+                                              bottomView: CardBottomSimpleDescriptionView(colorProvider: viewModel.colorProvider,
+                                                                                          descriptionText:
+                                                                                            Text("The maximal heartbeat was ")
+                                                                                          + Text("\(maxHeartRate) bpm").foregroundColor(viewModel.colorProvider.sleepyColorScheme.getColor(of: .heart(.heartColor))).bold()
+                                                                                          + Text(", while the minimal was ")
+                                                                                          + Text("\(minHeartRate) bpm").foregroundColor(viewModel.colorProvider.sleepyColorScheme.getColor(of: .heart(.heartColor))).bold()
+                                                                                          + Text(".")))
+                                .roundedCardBackground(color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .card(.cardBackgroundColor)))
+                        }
                     }
                 }
             }
         }
         .navigationTitle("Summary, \(Date().getFormattedDate(format: "MMM d"))")
         .onAppear {
-            let provider = viewModel.statisticsProvider
-
-            sleepStart = provider.getTodaySleepIntervalBoundary(boundary: .start)
-            sleepEnd = provider.getTodaySleepIntervalBoundary(boundary: .end)
-            sleepDuration = provider.getTodaySleepDuration()
-            fallAsleepDuration = provider.getTodayFallingAsleepDuration()
-            heartRateData = getShortHeartRateData(heartRateData: provider.getTodayData(of: .heart))
-
-            if let maxHR = provider.getData(dataType: .heart, indicatorType: .max),
-               let minHR = provider.getData(dataType: .heart, indicatorType: .min) {
-                maxHeartRate = Int(maxHR).description
-                minHeartRate = Int(minHR).description
-            } else {
-                maxHeartRate = "-"
-                minHeartRate = "-"
-            }
+            getSleepData()
+            getPhasesData()
+            getHeartData()
         }
         //        List(viewModel.cards ?? []) { card in
         //            HStack {
@@ -126,6 +122,40 @@ struct FeedListView: View {
         //                viewModel.open(card)
         //            }
         //        }
+    }
+
+    private func getHeartData() {
+        let provider = viewModel.statisticsProvider
+        heartRateData = getShortHeartRateData(heartRateData: provider.getTodayData(of: .heart))
+
+        if let maxHR = provider.getData(dataType: .heart, indicatorType: .max),
+           let minHR = provider.getData(dataType: .heart, indicatorType: .min) {
+            maxHeartRate = Int(maxHR).description
+            minHeartRate = Int(minHR).description
+        } else {
+            maxHeartRate = "-"
+            minHeartRate = "-"
+        }
+
+        if !heartRateData.isEmpty {
+            showHeartCard.toggle()
+        }
+    }
+
+    private func getPhasesData() {
+        let provider = viewModel.statisticsProvider
+        // TODO: Phases logic
+        if !phasesData.isEmpty {
+            showPhasesCard.toggle()
+        }
+    }
+
+    private func getSleepData() {
+        let provider = viewModel.statisticsProvider
+        sleepStart = provider.getTodaySleepIntervalBoundary(boundary: .start)
+        sleepEnd = provider.getTodaySleepIntervalBoundary(boundary: .end)
+        sleepDuration = provider.getTodaySleepDuration()
+        fallAsleepDuration = provider.getTodayFallingAsleepDuration()
     }
 
     private func getShortHeartRateData(heartRateData: [Double]) -> [Double] {
