5659b9a0434dca971e72db0fdd73e06f7dc31a69
Anas Ben Mustafa <57250290+arabianprince23@users.noreply.github.com>
2021-10-16 22:55:49 +0300

    Added background

--- Sleepy/Scenes/Root/Alarm/AlarmCoordinatorView.swift
+++ Sleepy/Scenes/Root/Alarm/AlarmCoordinatorView.swift
@@ -22,66 +22,70 @@ struct AlarmCoordinatorView: View {
     var body: some View {
         GeometryReader { g in
             NavigationView {
-                ScrollView {
-                    VStack (alignment: .leading) {
-                        Image("night")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .padding(.top)
-
-                        SectionNameTextView(
-                            text: "What is a smart alarm?".localized,
-                            color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .textsColors(.standartText))
-                        )
-                            .padding(.top)
-
-                        Text("What is a smart alarm description".localized)
-                            .padding([.trailing, .leading, .bottom])
-                            .padding(.top, Constant.smallTopPadding)
-
-                        SectionNameTextView(
-                            text: "Improve your sleep with smart alarm".localized,
-                            color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .textsColors(.standartText))
-                        )
-
-                        Text("Improve your sleep with smart alarm description".localized)
-                            .padding([.trailing, .leading, .bottom])
-                            .padding(.top, Constant.smallTopPadding)
-
-                        SectionNameTextView(
-                            text: "How to use smart alarm in Sleepy?".localized,
-                            color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .textsColors(.standartText))
-                        )
-
-                        Text("How to use smart alarm in Sleepy description".localized)
-                            .padding([.trailing, .leading])
-                            .padding(.top, Constant.smallTopPadding)
-
-                        HStack(alignment: .center) {
-                            Image("alarmMain")
+                ZStack {
+                    viewModel.colorProvider.sleepyColorScheme.getColor(of: .general(.appBackgroundColor))
+                        .edgesIgnoringSafeArea(.all)
+                    ScrollView {
+                        VStack (alignment: .leading) {
+                            Image("night")
                                 .resizable()
                                 .aspectRatio(contentMode: .fit)
-                                .frame(
-                                    width: g.size.width / 2,
-                                    height: Constant.appleWatchImageheight,
-                                    alignment: .center
-                                )
+                                .padding(.top)
 
-                            Image("alarmInside")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(
-                                    width: g.size.width / 2,
-                                    height: Constant.appleWatchImageheight,
-                                    alignment: .center
-                                )
+                            SectionNameTextView(
+                                text: "What is a smart alarm?".localized,
+                                color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .textsColors(.standartText))
+                            )
+                                .padding(.top)
+
+                            Text("What is a smart alarm description".localized)
+                                .padding([.trailing, .leading, .bottom])
+                                .padding(.top, Constant.smallTopPadding)
+
+                            SectionNameTextView(
+                                text: "Improve your sleep with smart alarm".localized,
+                                color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .textsColors(.standartText))
+                            )
+
+                            Text("Improve your sleep with smart alarm description".localized)
+                                .padding([.trailing, .leading, .bottom])
+                                .padding(.top, Constant.smallTopPadding)
+
+                            SectionNameTextView(
+                                text: "How to use smart alarm in Sleepy?".localized,
+                                color: viewModel.colorProvider.sleepyColorScheme.getColor(of: .textsColors(.standartText))
+                            )
+
+                            Text("How to use smart alarm in Sleepy description".localized)
+                                .padding([.trailing, .leading])
+                                .padding(.top, Constant.smallTopPadding)
+
+                            HStack(alignment: .center) {
+                                Image("alarmMain")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(
+                                        width: g.size.width / 2,
+                                        height: Constant.appleWatchImageheight,
+                                        alignment: .center
+                                    )
+
+                                Image("alarmInside")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(
+                                        width: g.size.width / 2,
+                                        height: Constant.appleWatchImageheight,
+                                        alignment: .center
+                                    )
+                            }
                         }
                     }
+                    .navigationBarTitle(
+                        "Smart alarm".localized,
+                        displayMode: .large
+                    )
                 }
-                .navigationBarTitle(
-                    "Smart alarm".localized,
-                    displayMode: .large
-                )
             }
         }
     }
