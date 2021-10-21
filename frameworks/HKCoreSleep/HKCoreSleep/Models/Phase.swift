acb0c4581aa8a1b16337759619d1097b4f919a80
Nikita Kazantsev <kaznik2013@gmail.com>
2021-10-21 22:32:21 +0300

    refactoring started

--- frameworks/HKCoreSleep/HKCoreSleep/Models/Phase.swift
+++ frameworks/HKCoreSleep/HKCoreSleep/Models/Phase.swift
@@ -6,9 +6,18 @@ public enum Condition {
     case deep
 }
 
+public struct SampleData {
+    public let date: Date
+    public let value: Double
+}
+
 public struct Phase {
     public let interval: DateInterval
     public let condition: Condition
+    public let heartData: [SampleData]
+    public let energyData: [SampleData]
+    public let breathData: [SampleData]
+
     public let verdictCoefficient: Double
     public let meanHeartRate: Double?
     public let chartPoint: Double
