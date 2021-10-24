3c83125a9bb7860014caf4194c393310ee829a57
Anas Ben Mustafa <57250290+arabianprince23@users.noreply.github.com>
2021-10-24 15:02:42 +0300

    Added new light phase detection algorithm to HeartDataManager

--- Sleepy_WatchOS Extension/Health/Heart/HeartDataManager.swift
+++ Sleepy_WatchOS Extension/Health/Heart/HeartDataManager.swift
@@ -7,29 +7,93 @@
 
 import Foundation
 
+typealias HeartRate = Double
+
+// MARK: Enums
+
+fileprivate enum LightPhaseDetectionStrategy {
+    case withConfirmation
+    case withoutConfirmation
+}
+
+fileprivate enum Constant {
+    static let confirmativeLightPhaseFlagValue: Double = 1.05
+    static let potentialLightPhaseFlagValue: Double = 1.08
+    static let unconditionalLightPhaseDetectionValue: Double = 1.12
+}
+
+// MARK: HeartDataManager
+
 final class HeartDataManager {
 
-    private var heartRates = [HeartRateSample]()
+    // MARK: Properties
+
     weak var delegate: HeartDataManagerDelegate?
+
+    private var heartRates = [HeartRate]()
+    private var potentialLightPhaseDetectionFlag: Bool = false
 
     // MARK: Internal methods
 
-    func append(sample: HeartRateSample) {
-        self.heartRates.append(sample)
-        isLightPhase(currentSample: sample)
-    }
-
-    func isLightPhase(currentSample: HeartRateSample) {
-        guard currentSample.heartRate / currentMeanValue() >= 1.2 else {
-            return
-        }
-        delegate?.lightPhaseDetected()
+    func append(sample: HeartRate) {
+        heartRates.append(sample)
+        detectLightPhase()
     }
 
     // MARK: Private methods
 
-    private func currentMeanValue() -> Double {
-        return heartRates.reduce(0.0) { $0 + $1.heartRate } / Double(heartRates.count)
+    private func detectLightPhase() {
+        guard
+            heartRates.count > 1,
+            let last = heartRates.last,
+            let preLast = heartRates.dropLast().last
+        else {
+            return
+        }
+
+        let differencePercentage = last / preLast
+        if differencePercentage >= Constant.unconditionalLightPhaseDetectionValue {
+            detectLightPhaseWithStrategy(.withoutConfirmation)
+        } else {
+            detectLightPhaseWithStrategy(.withConfirmation)
+        }
+    }
+
+    private func detectLightPhaseWithStrategy(_ strategy: LightPhaseDetectionStrategy) {
+        switch strategy {
+        case .withConfirmation:
+            if potentialLightPhaseDetectionFlag && isLightPhaseDetectionConfirmed() {
+                delegate?.lightPhaseDetected()
+            } else {
+                detectPotentialLightPhase()
+            }
+        case .withoutConfirmation:
+            delegate?.lightPhaseDetected()
+        }
+    }
+
+    private func detectPotentialLightPhase() {
+        guard
+            let last = heartRates.last,
+            let preLast = heartRates.dropLast().last,
+            last / preLast >= Constant.potentialLightPhaseFlagValue
+        else {
+            return
+        }
+        potentialLightPhaseDetectionFlag = true
+    }
+
+    private func isLightPhaseDetectionConfirmed() -> Bool {
+        guard
+            heartRates.count > 2,
+            let last = heartRates.last,
+            let prePreLast = heartRates.dropLast().dropLast().last,
+            last / prePreLast >= Constant.confirmativeLightPhaseFlagValue
+        else {
+            potentialLightPhaseDetectionFlag = false
+            return false
+        }
+        return true
     }
 
 }
